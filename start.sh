add35b76cd3c37e0d075e1bc3605589df67161c1
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-03-08 21:08:49 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/Cyniteofficial/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
-cd /Shortner-Converter-Bot-V2
+git clone https://github.com/Vijay14958/VJLink.git /VJLink
+cd /VJLink
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
