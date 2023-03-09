0097e0cdae2f42d212f93936543720a795ea8c5d
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-03-09 06:58:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ DATABASE_NAME = os.environ.get("DATABASE_NAME", "VJLinkShortner")
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://VJlink:VJLink@cluster0.ucnckh4.mongodb.net/?retryWrites=true&w=majority") 
 OWNER_ID =  int(os.environ.get("OWNER_ID", "5606411877")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(Id Owned Id)
+ADMINS.append(OWNER_ID)
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001716170358")) 
 UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "") # For Force Subscription
