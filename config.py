ead7e78368c4a3189dfe08db770b2580eb042938
Neha <116162193+Vijay14958@users.noreply.github.com>
2023-03-09 09:03:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(OWNER_ID)
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001716170358")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "") # For Force Subscription
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "-1001710985956") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
